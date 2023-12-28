6e483406316a9e4e1431f6d02f64036f540e313d
Koxalk <xalkiadakis1994@gmail.com>
2023-12-28 13:06:22 +0200

    commit

--- src/AnalogClock.java
+++ src/AnalogClock.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.*;
 
 public class AnalogClock extends JPanel {
